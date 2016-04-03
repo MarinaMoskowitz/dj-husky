298f02037003220ae936cac64a47b65b39218a6a
Brendon Wong <wong.bre@husky.neu.edu>
2016-04-03 05:20:11 -0400

    Cleaning up POST

--- djhuskyapp/static/js/main.js
+++ djhuskyapp/static/js/main.js
@@ -65,10 +65,6 @@ $(document).ready(function() {
             var id = $(this.innerHTML).attr('id');
             var title = $(this).find('.title').text();
             var artist = $(this).find('.artist').text();
-            console.log(id);
-            console.log(title);
-            console.log(artist);
-
             addToQueue(title, artist, id);
         });
 
@@ -85,14 +81,13 @@ $(document).ready(function() {
 
         console.log(JSON.stringify(data));
 
-        //$.ajax({
-        //    type: "POST",
-        //    url:"/api/songs/",
-        //    data: JSON.stringify(data),
-        //    success: function() {alert("success");}
-        //});
-
-        $.post("/api/songs/", JSON.stringify(data));
+        $.ajax({
+            type: "POST",
+            url:"/api/songs/",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            success: function() {alert("success");}
+        });
     }
 
     function addToVisualQueue(name, artist) {
