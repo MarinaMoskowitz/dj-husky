da807a6f0ed5854bc068755d7ee5f5e8a62741ef
Marina Moskowitz <marinamoskowitz@Marinas-MacBook-Pro-2.local>
2016-04-03 09:49:31 -0400

    Finishing toggle

--- djhuskyapp/models.py
+++ djhuskyapp/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth import User
 from django.db import models
 
 
@@ -20,12 +21,32 @@ class Song(models.Model):
     song_id = models.AutoField(primary_key=True)
     party = models.ForeignKey(Party, related_name='songs', on_delete=models.CASCADE)
     track_id = models.CharField(max_length=100)
-    upvotes = models.IntegerField(default=0)
-    downvotes = models.IntegerField(default=0)
     played = models.BooleanField(default=False)
+    upVote = models.IntegerField(default=0)
+    downVote = models.IntegerField(default=0)
 
     class Meta:
         unique_together = ('party', 'track_id')
 
     def __unicode__(self):
         return '{0}, {1}, {2}, {3}, {4}'.format(self.song_id, self.upvotes, self.downvotes, self.track_id, self.played)
+
+    button = gtk.ToggleButton(label=None)
+    button.set_active(True)
+    button.connect("toggled", self.update_count, "1")
+
+    button = gtk.ToggleButton(label=None)
+    button.connect("toggled", self.update_count, "1")
+
+    def update_count(self, button, ):
+        if button.get_active():
+            return upVote + 1
+        else:
+            return downVote + 1
+
+
+
+
+
+
+
