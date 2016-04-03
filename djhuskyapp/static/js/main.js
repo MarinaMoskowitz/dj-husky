91f431a8f4bcab52cf02bdd399b58357306d8d9e
Sean Johnsen <seanjohnsen96@gmail.com>
2016-04-03 03:49:02 -0400

    i don't know

--- djhuskyapp/static/js/main.js
+++ djhuskyapp/static/js/main.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
 
+
+
     $.ajax({
         url: "/api/parties/21/?format=json"
     }).success(function(response) {
 
         var party_title = response.name;
-        $("#partyname").val("Welcome to " + party_title + "!");
+        $("#partyname").html(party_title);
+
         console.log(JSON.stringify(party_title));
     });
-    
+
+
+
 
     $(".sidebar.left").sidebar();
 
@@ -52,37 +57,35 @@ $(document).ready(function() {
         }
     });
 
-    $('#results').selectable();
 
-    function addToDB() {
-        $.post('http://localhost:8000/api/parties/'),
-        {
-            'name':'Hooray'
-        },
-            function() {
-                console.log("It worked!");
-            }
-    }
+    $('#results').selectable();
 
     $("#button").click(function() {
 
         var selected_songs = $(".ui-selected");
-
+        console.log(selected_songs);
+        selected_songs.each(function() {
+            var title = $(this).attr();
+            console.log($(this).text());
+        });
         for (var i= 0, len=selected_songs.length; i < len; i++) {
-            console.log(selected_songs[i]);
+            addToQueue("test", "TEST", "123");
         }
     });
 
     function addToQueue(name, artist, id) {
         var listToAddTo = $("#queue");
-        var result = "<div class='containerthing'>" +
-            "<img src='../static/img/up-arrow.png' width='20px' height='20px'>" +
-            "<img src='../static/img/down-arrow.png' width='20px' height='20px'>" +
-            "<div class='song'>" +
-            "<p class='title'>" + name + "</p>" +
-            "<p class='artist'>" + artist + "</p>" +
-            "</div>" +
-            "<div style='clear: both;'></div>";
-        listToAddTo.html(result);
+        var result = [
+            "<div class='containerthing'>",
+            "   <img src='../static/img/up-arrow.png' width='20px' height='20px'>",
+            "   <img src='../static/img/down-arrow.png' width='20px' height='20px'>",
+            "   <div class='song'>",
+            "       <p class='title'>" + name + "</p>",
+            "       <p class='artist'>" + artist + "</p>",
+            "   </div>",
+            "</div>",
+            "<div style='clear: both;'></div>"].join("\n");
+        listToAddTo.append(result);
     }
+
 });
